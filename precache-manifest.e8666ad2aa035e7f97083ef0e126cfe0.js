7070ecbfe4f70e4ef1e86baa32cbfbe8218297a5
01141920041 <ezemsm@gmail.com>
2020-10-21 14:10:13 -0500

    Updates

---
+++ precache-manifest.e8666ad2aa035e7f97083ef0e126cfe0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c44a368cb07c38f7d9d4c7a494ff9b7",
+    "url": "/github.io/monstersUdemy/index.html"
+  },
+  {
+    "revision": "8026a7b0bb7358fe4081",
+    "url": "/github.io/monstersUdemy/static/css/main.aaff85a8.chunk.css"
+  },
+  {
+    "revision": "88b685495fee72b0273c",
+    "url": "/github.io/monstersUdemy/static/js/2.2eaf0afe.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/github.io/monstersUdemy/static/js/2.2eaf0afe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8026a7b0bb7358fe4081",
+    "url": "/github.io/monstersUdemy/static/js/main.f62d4ac9.chunk.js"
+  },
+  {
+    "revision": "ccae8a583f913e0020ca",
+    "url": "/github.io/monstersUdemy/static/js/runtime-main.00615bdf.js"
+  }
+]);
